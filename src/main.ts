84f0f2fea030ec7e158cefeadb53093377482e68
Jeremy Koster <jeremy@biesbroeck.eu>
2023-05-01 14:00:53 +0200

    axios installed en cors aangegeven

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,9 @@ async function bootstrap() {
   const app: NestExpressApplication = await NestFactory.create(AppModule);
   const config: ConfigService = app.get(ConfigService);
   const port: number = config.get<number>('PORT');
+  app.enableCors({
+    origin: '*',
+  });
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   
